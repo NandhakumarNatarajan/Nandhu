5c9bd383c4f1cf3fdb44271dcdbeec49912642a0
NandhakumarNatarajan <nandhun96@gmail.com>
2017-04-19 14:21:25 +0530

    Add files via upload

---
+++ nandhu.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a==0)
+printf("the value is zero");
+else if(a>=1)
+printf("the value is possitive");
+else
+printf("the value is negative");
+}
