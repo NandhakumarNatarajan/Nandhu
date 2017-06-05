6de6c6c3409dbc5eb141413c2f0a268ecfd43695
NandhakumarNatarajan <nandhun96@gmail.com>
2017-06-05 13:58:26 +0530

    Add files via upload

---
+++ Digits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Digits
+{
+public static void main(String args[])
+{
+	System.out.println("enter the string");
+	Scanner input=new Scanner(System.in);
+	String str= input.next();
+	StringBuffer s1=new StringBuffer(str);
+	StringBuffer s2=s1.reverse();
+	System.out.println(s2);
+	
+	
+	
+}
+}
