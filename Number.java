9ff8b3a84db229ff5b012a38f60002eb76945207
NandhakumarNatarajan <nandhun96@gmail.com>
2017-06-05 14:26:13 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Number {
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("enter the string");
+		String str=in.next();
+		int num=Integer.parseInt(str);
+		System.out.println(num);
+		
+	}
+
+}
